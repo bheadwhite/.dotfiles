61a9ee70dd718b13fdaedd117a7eae5da47a222d
brent.whitehead <bheadwhite@gmail.com>
2024-08-23 17:59:12 -0600

    added tailwind

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -118,6 +118,18 @@ return {
     end,
   },
   { "AckslD/messages.nvim", config = true }, -- messages
+  -- tailwind-tools.lua
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+      "neovim/nvim-lspconfig", -- optional
+    },
+    opts = {}, -- your configuration
+  },
   -- {
   -- 	"numToStr/Comment.nvim",
   -- 	opts = {
