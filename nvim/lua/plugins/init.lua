48ad21b25ef2d5dc4f2374f61d0b047e03a2cd24
Brent <brent.whitehead@tcnbroadcasting.com>
2024-09-10 15:27:56 -0600

    added tailwind check

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -11,13 +11,13 @@ return {
     "nvim-treesitter/nvim-treesitter-textobjects",
     "nvim-treesitter/playground",
   },
-  {
-    "andymass/vim-matchup",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      vim.g.matchup_matchparen_offscreen = { method = "popup" }
-    end,
-  },
+  -- {
+  --   "andymass/vim-matchup",
+  --   dependencies = { "nvim-treesitter/nvim-treesitter" },
+  --   config = function()
+  --     vim.g.matchup_matchparen_offscreen = { method = "popup" }
+  --   end,
+  -- },
   {
     "olimorris/persisted.nvim",
     lazy = false,
@@ -81,7 +81,7 @@ return {
     "dmmulroy/tsc.nvim", -- Typescript
     opts = {
       use_diagnostics = true,
-      run_as_monorepo = true,
+      -- run_as_monorepo = true,
     },
   },
   { "s1n7ax/nvim-window-picker" }, -- window picker
@@ -174,12 +174,31 @@ return {
     config = function()
       require("tailwind-tools").setup()
 
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*.css,*.scss,*.sass,*.less,*.styl,*.html,*.js,*.ts,*.jsx,*.tsx,*.vue",
-        callback = function()
-          vim.cmd("TailwindSortSync")
-        end,
-      })
+      local function file_exists(name)
+        local f = io.open(name, "r")
+        if f ~= nil then
+          io.close(f)
+          return true
+        else
+          return false
+        end
+      end
+
+      -- Get the current working directory
+      local cwd = vim.loop.cwd()
+
+      -- only add this if we have a tailwind.config.js file in the root of the project
+      if file_exists(cwd .. "/tailwind.config.js") then
+        print("yes")
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          pattern = "*.css,*.scss,*.sass,*.less,*.styl,*.html,*.js,*.ts,*.jsx,*.tsx,*.vue",
+          callback = function()
+            vim.cmd("TailwindSortSync")
+          end,
+        })
+      else
+        print("no")
+      end
     end,
   },
   -- {
