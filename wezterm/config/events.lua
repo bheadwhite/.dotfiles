d1741e3e42d1496d9e4087153e88d47cf691224f
Brent <brent.whitehead@tcnbroadcasting.com>
2025-04-08 05:57:50 -0600

    just clock

--- wezterm/config/events.lua
+++ wezterm/config/events.lua
@@ -44,13 +44,8 @@ function M.setRightStatus(window, pane)
 	-- "Wed Mar 3 08:14"
 	local date = wezterm.strftime("%a %b %-d %H:%M ")
 
-	local bat = ""
-	for _, b in ipairs(wezterm.battery_info()) do
-		bat = "🔋 " .. string.format("%.0f%%", b.state_of_charge * 100)
-	end
-
 	window:set_right_status(wezterm.format({
-		{ Text = bat .. "   " .. date },
+		{ Text = date },
 	}))
 end
 
